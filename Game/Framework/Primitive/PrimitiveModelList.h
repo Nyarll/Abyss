f786ffaa8ed279d7ab037c04b6e7e3194710a166
Nyarll <oss314y08@i.softbank.jp>
2020-06-02 11:15:40 +0900

    update : rename

---
+++ Game/Framework/Primitive/PrimitiveModelList.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// <class name		: PrimitiveModelList>
+// <update date		: 2020 / 06 / 02>
+// <author			: Nyarll>
+// <GitHub			: https://github.com/Nyarll >
+
+#ifndef PRIMITIVEMODELLIST_DEFINED
+#define PRIMITIVEMODELLIST_DEFINED
+
+#endif
