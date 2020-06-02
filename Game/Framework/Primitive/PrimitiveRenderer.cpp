a900e7130fc64aa08aebc6050de9a110935fac5e
Nyarll <oss314y08@i.softbank.jp>
2020-06-02 21:52:12 +0900

    update : PrimitiveRenderer

--- Game/Framework/Primitive/PrimitiveRenderer.cpp
+++ Game/Framework/Primitive/PrimitiveRenderer.cpp
@@ -1,1 +1,12 @@
 #include "PrimitiveRenderer.h"
+
+void PrimitiveRenderer::SetModel(PrimitiveModel * model)
+{
+	m_modelPtr = model;
+}
+
+void PrimitiveRenderer::Draw(const DirectX::SimpleMath::Matrix& world, const DirectX::SimpleMath::Matrix& view,
+	const DirectX::SimpleMath::Matrix& projection, DirectX::XMVECTORF32 color)
+{
+	m_modelPtr->Draw(world, view, projection, color);
+}
--- Game/Framework/Primitive/PrimitiveRenderer.h
+++ Game/Framework/Primitive/PrimitiveRenderer.h
@@ -11,7 +11,15 @@
 // <PrimitiveRenderer>
 class PrimitiveRenderer
 {
+private:
+	using PrimitiveModel = DirectX::GeometricPrimitive;
+	PrimitiveModel* m_modelPtr;
 
+public:
+	void SetModel(PrimitiveModel* model);
+
+	void Draw(const DirectX::SimpleMath::Matrix& world, const DirectX::SimpleMath::Matrix& view,
+		const DirectX::SimpleMath::Matrix& projection, DirectX::XMVECTORF32 color);
 };
 
 #endif
